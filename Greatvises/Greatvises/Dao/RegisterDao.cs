bacb03055820e50f54291dcdf4b7cb3f3f15c553
Diana Bonilla <dianapamela3197@gmail.com>
2018-09-21 21:45:44 -0600

    Api2.0 Terminada

---
+++ Greatvises/Greatvises/Dao/RegisterDao.cs
@@ -0,0 +1,69 @@
+﻿using Greatvises.Models;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace Greatvises.Dao
+{
+    public class RegisterDao
+    {
+        private MongoClient mongoClient;
+        private IMongoCollection<Client> clientCollection;
+
+
+        public RegisterDao()
+        {
+             mongoClient = new MongoClient(ConfigurationManager.AppSettings["mongoDBHost"]);
+            var db = mongoClient.GetDatabase(ConfigurationManager.AppSettings["mongoDBName"]);
+            clientCollection = db.GetCollection<Client>("client");
+        }
+
+
+        public void RegisterClient(Client client)
+        {
+            clientCollection.InsertOne(client);
+
+        }
+
+        public void insertTransactionKey(Client client)
+        {
+            var filter = Builders<Client>.Filter.Eq(clientTemp => clientTemp.Password, client.Password);
+
+            clientCollection.UpdateOne(filter, Builders<Client>.Update
+                .Set("name", client.Name)
+                .Set("Password", client.Password)
+                .Set("transaction", client.TransactionPassword));
+        }
+
+        public string searchKey(string password)
+        {
+
+           List<Client> lista= clientCollection.AsQueryable<Client>().ToList();
+            foreach (Client client in lista ){
+                if (client.Password.Equals(password))
+                {
+                    return client.Name;
+                }
+            }
+            return "null";
+        }
+
+        public string searchTransactionKey(string transactionPassword)
+        {
+
+            List<Client> lista = clientCollection.AsQueryable<Client>().ToList();
+            foreach (Client client in lista)
+            {
+                if (client.TransactionPassword.Equals(transactionPassword))
+                {
+                    return client.Name;
+                }
+            }
+            return "null";
+
+        }
+    }
+}
---
+++ Greatvises/Greatvises/Models/Bill.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Greatvises.Models
+{
+    public class Bill
+    {
+
+        string nameClient;
+        Product product;
+
+        public Bill(string nameClient, Product product)
+        {
+            this.NameClient = nameClient;
+            this.Product = product;
+        }
+
+        public string NameClient { get => nameClient; set => nameClient = value; }
+        public Product Product { get => product; set => product = value; }
+    }
+}
